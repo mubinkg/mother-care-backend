f9a81279212a928d5f4344bb4d23975612e9763f
Mubin-Mohammad-Ikhtiar-Khan <mubin.ice.ru@gmail.com>
2023-08-08 12:27:40 +0600

    remove cors from app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*', // This allows requests from all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable credentials if needed
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
   console.log(`app started on http://localhost:3000`)
 }
